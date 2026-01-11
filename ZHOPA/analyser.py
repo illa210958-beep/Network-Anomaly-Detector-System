94bc812d5a13666520a2427b5235a9e3228794c1
Ozor_noy <illa210958@mail.ru>
2026-01-11 15:34:43 +0300

    Delete ZHOPA directory

--- ZHOPA/analyser.py
+++
@@ -1,9 +0,0 @@
-from main import NetworkAnomalyDetector
-
-filename = input("Введите имя PCAP файла (например: 1.pcap): ").strip()
-
-detector = NetworkAnomalyDetector()
-results = detector.analyze_pcap(filename, model_type='random_forest')
-
-# Анализируем с Isolation Forest (если нужно)
-# results = detector.analyze_pcap(filename, model_type='isol1p2ation_forest')
